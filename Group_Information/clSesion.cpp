1b060db45fb2251218b5ad7a79066dd6228adaec
Alexander <87269586+medlcago@users.noreply.github.com>
2023-05-29 13:41:21 +0300

    Delete clSesion.cpp

--- Group_Information/clSesion.cpp
+++
@@ -1,4 +0,0 @@
-#include "clSesion.h"
-
-clSesion::clSesion(std::string discipline_name, clDate submission_date, int grade, std::string teacher) :
-    discipline_name(discipline_name), submission_date(submission_date), grade(grade), teacher(teacher) {}
